f810b4e17ccaca012633f15edb254c9311ad7fe0
Markus Klemann <mklemann@web.de>
2022-05-01 03:48:29 +0200

    feat: setup rest client

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
-  name: string;
+interface IApiHelloRequest extends NextApiRequest {
+  body: { name?: string };
+}
+
+export type IApiHelloResponseData = {
+  name?: string;
 };
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<IApiHelloResponseData>
 ) {
-  res.status(200).json({ name: 'John Doe' });
+  const {
+    body: { name },
+  } = req;
+  if (req.method === 'POST' && name && name.length > 0) {
+    res.status(200).json({ name });
+  } else {
+    res.status(400).json({});
+  }
 }
